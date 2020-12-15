0f0a0c8804f914d8cc19b9a5dd4f6abd4942eeef
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-12-16 01:38:42 +0900

    新規投稿機能作成

--- app/controllers/miniblogs_controller.rb
+++ app/controllers/miniblogs_controller.rb
@@ -1,2 +1,31 @@
 class MiniblogsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+  def index
+    
+  end
+
+  def new
+    @miniblog = Miniblog.new
+  end
+
+  def create
+    @miniblog = Miniblog.new(miniblog_params)
+    if @miniblog.valid?
+      @miniblog.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def miniblog_params
+    params.require(:miniblog).permit(:title, :text, images: []).merge(user_id: current_user.id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,41 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
+
+      if (imageElementNum <= 1 ) {
+        const blobImage = document.createElement('img');
+        blobImage.setAttribute('src', blob);
+        
+        const inputHTML = document.createElement('input')
+        inputHTML.setAttribute('id', `message_image_${imageElementNum}`)
+        inputHTML.setAttribute('name', 'message[images][]')
+        inputHTML.setAttribute('type', 'file')
+
+        imageElement.appendChild(blobImage);
+        imageElement.appendChild(inputHTML)
+        ImageList.appendChild(imageElement);
+  
+        inputHTML.addEventListener('change', (e) => {
+          file = e.target.files[0];
+          blob = window.URL.createObjectURL(file);
+  
+          createImageHTML(blob)
+        })
+      } else{
+        return false;
+      }
+    };
+
+    document.getElementById('miniblog-image').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
+    });
+  });
+}
