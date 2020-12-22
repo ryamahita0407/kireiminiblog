51519e25f9400410c3efc5e237b6501b7ead654d
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-12-22 16:04:42 +0900

    アソシエーション及びバリデーション定義

--- app/models/miniblog.rb
+++ app/models/miniblog.rb
@@ -2,6 +2,8 @@ class Miniblog < ApplicationRecord
   belongs_to :user
   has_many_attached :images
   has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   
   with_options presence: true do
     validates :title, :text, :images
